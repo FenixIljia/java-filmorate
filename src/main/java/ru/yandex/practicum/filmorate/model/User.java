7556625e28df0fedaf80810dc4ba750821cd191b
Илья <iljiafenix@yandex.ru>
2025-05-11 22:18:35 +0300

    feat: со0дан каркас приложение и основные поля в классах User и Film

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -0,0 +1,11 @@
+package ru.yandex.practicum.filmorate.model;
+
+import java.time.LocalDateTime;
+
+public class User {
+    long id;
+    String email;
+    String login;
+    String name;
+    LocalDateTime birthday;
+}
