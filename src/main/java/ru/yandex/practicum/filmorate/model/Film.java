11186251290cb0a825865ae902e18a33756885c3
Илья <iljiafenix@yandex.ru>
2025-05-11 22:18:24 +0300

    feat: со0дан каркас приложение и основные поля в классах User и Film

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -3,10 +3,18 @@ package ru.yandex.practicum.filmorate.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+
 /**
  * Film.
  */
 @Getter
 @Setter
 public class Film {
+    long id;
+    String name;
+    String description;
+    LocalDateTime releaseDate;
+    Duration duration;
 }
