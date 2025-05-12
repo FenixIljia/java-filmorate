f1782328cb7d5fe1ad5b810c18e5cceb5605659c
Илья <iljiafenix@yandex.ru>
2025-05-12 16:33:11 +0300

    feat: добавлено логирование

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,20 +1,20 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.exception.DuplicatedDataException;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/films")
+@Slf4j
 public class FilmController {
 
     private final Map<Long, Film> films = new HashMap<>();
@@ -27,15 +27,14 @@ public class FilmController {
     @PostMapping
     public Film create(@RequestBody @Valid Film film) {
         if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
+            log.warn("Попытка добавления фильма с датой релиза раньше 28 декабря 1895 года. Дата релиза фильма - {}.",
+                    film.getReleaseDate()
+            );
             throw new ValidationException("Дата релиза должна быть не раньше 28 декабря 1895 года");
-        }
-        for (Film filmName : films.values()) {
-            if (film.getName().equals(filmName.getName())) {
-                throw new DuplicatedDataException("Фильм " + film.getName() + " уже существует");
-            }
         }
         film.setId(getNextId());
         films.put(film.getId(), film);
+        log.info("Добавлен новый фильм - {}.", film.getName());
         return film;
     }
 
@@ -43,10 +42,17 @@ public class FilmController {
     public Film update(@RequestBody @Valid Film film) {
         if (film.getReleaseDate() != null) {
             if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
+                log.warn(
+                        "Попытка установить недопустимую дату релизу фильма(" +
+                                "дата релиза фильма должна быть не раньше 28 декабря 1895 года)." +
+                                " Дата релиза фильма - {}.",
+                        film.getReleaseDate()
+                );
                 throw new ValidationException("Дата релиза должна быть не раньше 28 декабря 1895 года");
             }
         }
         if (!films.containsKey(film.getId())) {
+            log.warn("Попытка обновить фильм, которого нет в базе. id фильм - ${}.", film.getId());
             throw new NotFoundException("Фильм с id " + film.getId() + " не найден");
         }
 
@@ -54,16 +60,21 @@ public class FilmController {
 
         if (film.getReleaseDate() != null) {
             oldFilm.setReleaseDate(film.getReleaseDate());
+            log.debug("Дата релиза фильма {} обновлена на: {}.", oldFilm.getName(), oldFilm.getReleaseDate());
         }
         if (film.getDescription() != null) {
             oldFilm.setDescription(film.getDescription());
+            log.debug("Описание фильма {} обновлено на: {}.", oldFilm.getName(), oldFilm.getDescription());
         }
         if (film.getDuration() != null) {
             oldFilm.setDuration(film.getDuration());
+            log.debug("Продолжительность фильма {} обновлена на: {}.", oldFilm.getName(), oldFilm.getDuration());
         }
         if (film.getName() != null) {
+            log.debug("Название фильма {} изменено на: {}.", oldFilm.getName(), film.getName());
             oldFilm.setName(film.getName());
         }
+        log.info("Фильма с id ${} успешно обновлен", oldFilm.getId());
         return oldFilm;
     }
 
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.filmorate.controller;
 
 import jakarta.validation.Valid;
 import jakarta.validation.ValidationException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.User;
@@ -12,6 +13,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/users")
+@Slf4j
 public class UserController {
 
     private final Map<Long, User> users = new HashMap<>();
@@ -25,20 +27,28 @@ public class UserController {
     public User create(@RequestBody @Valid User user) {
         for (User value : users.values()) {
             if (value.getEmail().equals(user.getEmail())) {
+                log.warn("Попытка добавить нового пользователя с email, который уже есть в базе - {}.", user.getEmail());
                 throw new ValidationException("Пользователь с email " + user.getEmail() + " уже существует");
             }
         }
         if (user.getName() == null) {
             user.setName(user.getLogin());
+            log.debug(
+                    "У нового пользователя {} не заполнено поле name. Полю name присвоено заначение поля login - {}.",
+                    user.getLogin(),
+                    user.getLogin()
+            );
         }
         user.setId(getNextId());
         users.put(user.getId(), user);
+        log.info("Добавлен новый пользователь - {}", user.getLogin());
         return user;
     }
 
     @PutMapping
     public User update(@RequestBody @Valid User user) {
         if (!users.containsKey(user.getId())) {
+            log.warn("Пользователь с id ${} не найден в базе", user.getId());
             throw new NotFoundException("Пользователь с id " + user.getId() + " не найден");
         }
 
@@ -46,16 +56,21 @@ public class UserController {
         if (user.getLogin() != null) {
             if (oldUser.getName().equals(oldUser.getLogin())) {
                 oldUser.setName(user.getLogin());
+                log.debug("У пользователя {} обновлено имя - {}.", oldUser.getLogin(), oldUser.getName());
             }
+            log.debug("У пользователя {} обновлен логин - {}.", oldUser.getLogin(), user.getLogin());
             oldUser.setLogin(user.getLogin());
         }
         if (user.getBirthday() != null) {
             oldUser.setBirthday(user.getBirthday());
+            log.debug("У пользователя {} обновлена дата рождения - {}.", oldUser.getLogin(), oldUser.getBirthday());
         }
         if (user.getName() != null) {
             oldUser.setName(user.getName());
+            log.debug("У пользователя {} обновлена дата рождения - {}.", oldUser.getLogin(), oldUser.getBirthday());
         }
         oldUser.setEmail(user.getEmail());
+        log.info("Данные пользователя {} успешно обновлены!", oldUser.getLogin());
         return oldUser;
     }
 
