cb26882061185768d41a6c27dcdc2034ca2428c8
Илья <iljiafenix@yandex.ru>
2025-06-30 05:07:06 +0300

    fix: исправлен checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/dal/UserDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dal/UserDbStorage.java
@@ -1,14 +1,12 @@
 package ru.yandex.practicum.filmorate.dal;
 
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
-import ru.yandex.practicum.filmorate.dal.mappers.UserRowMapper;
 import org.springframework.stereotype.Repository;
+import ru.yandex.practicum.filmorate.dal.mappers.UserRowMapper;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.User;
 
 import java.sql.Date;
-import java.sql.Timestamp;
 import java.util.List;
 import java.util.Optional;
 
